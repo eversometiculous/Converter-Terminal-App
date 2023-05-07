7765ce8a14e9ced6f940695c7bf6b7ac804d6d69
eversometiculous <14371@coderacademy.edu.au>
2023-05-07 12:29:55 +1000

    Added readme and main.py. Added import packages

---
+++ README.md
@@ -0,0 +1,1 @@
+# Converter-Terminal-App
---
+++ main.py
@@ -0,0 +1,11 @@
+# Import the packages needed to create this application
+import argparse
+import os
+import markdown
+import pdfkit
+import PyPDF2
+import html2text
+
+# Create the formats with their extensions
+
+
