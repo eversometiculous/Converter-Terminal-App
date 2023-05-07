e3d6749635318ef7092b32b79a52c6f33d805f6c
eversometiculous <14371@coderacademy.edu.au>
2023-05-07 13:27:06 +1000

    Added argparse arguments for input file, output file and output format with help

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ FORMATS = {
     'pdf': 'pdf'
 }
 
+parser = argparse.ArgumentParser(description="Converts between document formats") #default coding for argparse
+parser.add_argument("input_file", help="the input file name to convert")
+parser.add_argument("output_format", choices=FORMATS.keys(), help="the output format or extension you want")
+parser.add_argument("output_file", help="the output file name")
+
+
+args = parser.parse_args() # Default coding for argparse
+
