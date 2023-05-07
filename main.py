689dacd43afd87cbe2aa0753989cfaae0fd6659f
eversometiculous <14371@coderacademy.edu.au>
2023-05-07 14:15:05 +1000

    Added input format code

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import html2text
 
 # Create the formats with their extensions
 
-FORMATS = {
+formats = {
     'markdown': 'md',
     'html': 'html',
     'pdf': 'pdf'
@@ -16,9 +16,17 @@ FORMATS = {
 
 parser = argparse.ArgumentParser(description="Converts between document formats") #default coding for argparse
 parser.add_argument("input_file", help="the input file name to convert")
-parser.add_argument("output_format", choices=FORMATS.keys(), help="the output format or extension you want")
+parser.add_argument("output_format", choices=formats.keys(), help="the output format or extension you want")
 parser.add_argument("output_file", help="the output file name")
 
 
 args = parser.parse_args() # Default coding for argparse
 
+input_format = None
+for format, ext in formats.items():
+    if args.input_file.endswith("." + ext):
+        input_format = format
+        break
+
+if input_format is None:
+    raise ValueError(f"Unsupported input file format: {args.input_file}")
