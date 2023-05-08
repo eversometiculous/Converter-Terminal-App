618a68696f98136d5591a930a1cdee550f0ea76b
eversometiculous <14371@coderacademy.edu.au>
2023-05-08 16:42:45 +1000

    Modded executable script

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 python3 -m venv converter-venv
 source converter-venv/bin/activate
-pip3 install -r requirements.txt
+pip3 install argparse markdown pdfkit PyPDF2 html2text
 clear
 python3 main.py
