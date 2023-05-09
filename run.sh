2f3ea54d9f1e2c199b8748284aec04d82eaecf69
eversometiculous <14371@coderacademy.edu.au>
2023-05-09 11:45:50 +1000

    Cleaned up a bit of code

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ python3 -m venv converter-venv
 source converter-venv/bin/activate
 pip3 install -r requirements.txt
 clear
-python3 main.py
+python3 main.py #*insert inputfile_name.ext* *insert format you want* *insert file_name you want*
